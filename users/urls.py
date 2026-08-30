39269da58e451760a554478fe2b5132dea53638c
alishanepal000-ship-it <alishanepal000@gmail.com>
2026-08-30 09:21:35 +0545

    made JWT actually protect an API

--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import RegisterView
+from .views import RegisterView, ProfileView
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-
+    
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .serializers import RegisterSerializer
 
 # Create your views here.
@@ -8,3 +12,13 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email,
+        })
